7d227463867a9d8ac0b1b6ac41a18e596a90eb2b
Derek Garnett <rizzagum@gmail.com>
2024-07-28 19:31:00 +0000

    FEAT adds bird_tracker app and basic Http function

--- raptor_health/urls.py
+++ raptor_health/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from bird_tracker.views import bird_detail
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('bird_tracker/', bird_detail, name='bird_detail')
 ]
