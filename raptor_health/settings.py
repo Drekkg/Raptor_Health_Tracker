8f815be85f637341ef5ec54b3f03da1c42a1d885
Derek Garnett <rizzagum@gmail.com>
2024-07-30 22:25:20 +0000

    FIX debug false

--- raptor_health/settings.py
+++ raptor_health/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'Baroness42red'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '8000-drekkg-raptorhealthtrac-439x53tjdnt.ws.codeinstitute-ide.net',
