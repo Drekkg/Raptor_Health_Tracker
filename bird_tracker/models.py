e9abd9dcef16d1f7cf3b8a6b7f05db7347f41776
drekkg <rizzagum@gmail.com>
2025-11-24 21:28:27 +0100

    CHANGE updates DailyData model to set date field to auto_now_add; modifies behaviour field choices to include 'Neutral'

--- bird_tracker/models.py
+++ bird_tracker/models.py
@@ -43,7 +43,7 @@ class DailyData(models.Model):
         Bird, on_delete=models.CASCADE, related_name='selected_bird')
     trainer = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='trainer')
-    date = models.DateTimeField(auto_now=True, null=False,)
+    date = models.DateTimeField(auto_now_add=True, null=False,)
     weight = models.PositiveSmallIntegerField(null=False)
     food_type = models.CharField(max_length=12, blank=False)
     food_weight = models.PositiveSmallIntegerField(null=False)
