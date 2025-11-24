ff44fb2488f372502f4d40225ad734433e1aeb89
drekkg <rizzagum@gmail.com>
2025-11-24 19:41:28 +0100

    CHANGE adds 'Neutral' option to bird behaviour model; updates daily data form layout and enhances calendar styles in CSS; refactors calendar JavaScript for improved date handling

--- static/js/calendar.js
+++ static/js/calendar.js
@@ -33,34 +33,32 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 })
 
-// initiate and display the calendar
-   document.addEventListener('DOMContentLoaded', () => {
-        const { Calendar } = window.VanillaCalendarPro;
-        
+document.addEventListener('DOMContentLoaded', () => {
+  const { Calendar } = window.VanillaCalendarPro;
 
-        const calendar = new Calendar('#calendar', {
-            onCreateDateEls(self, dateEl) {
-                const btnEl = dateEl.querySelector('[data-vc-date-btn]');
-                if (!btnEl) return;
-            
-                // Get the full date of the current button
-                const fullDate = dateEl.getAttribute('data-vc-date');
-          
-                // Target a specific date (e.g., "2025-11-11" to aplly styles if training has occurred               
-                targetDate.forEach((targetDate) => {
-                if (fullDate === targetDate) {
-                    // Apply custom styles to the specific date
-                    btnEl.style.backgroundColor = 'green'; // Set background color
-                    btnEl.style.color = '#FFFFFF'; // Set text color
-                    btnEl.style.borderRadius = '10%'; // Make it circular
-                    btnEl.style.fontWeight = 'bold'; // Make the text bold
-                }
-              })
-            },
-        });
-       
-          
-        calendar.init();
-    });
+  // Calendar options
+  const options = {
+      selectedTheme: 'light',
+      onCreateDateEls(self, dateEl) {
+          const btnEl = dateEl.querySelector('[data-vc-date-btn]');
+          if (!btnEl) return;
 
+          // Get the full date of the current button
+          const fullDate = dateEl.getAttribute('data-vc-date');
 
+          // Apply styles to specific dates
+          targetDate.forEach((targetDate) => {
+              if (fullDate === targetDate) {
+                  btnEl.style.backgroundColor = 'green'; // Set background color
+                  btnEl.style.color = '#FFFFFF'; // Set text color
+                  btnEl.style.borderRadius = '10%'; // Make it circular
+                  btnEl.style.fontWeight = 'bold'; // Make the text bold
+              }
+          });
+      },
+  };
+
+  // Initialize the calendar
+  const calendar = new Calendar('#calendar', options);
+  calendar.init();
+});
