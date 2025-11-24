bce9e05f0c75ed693628c6164028b29bbe38f8f0
drekkg <rizzagum@gmail.com>
2025-11-24 20:50:53 +0100

    CHANGE adds support for styling dates with and without training in calendar; introduces targetDateNoTraining array for improved date handling

--- static/js/calendar.js
+++ static/js/calendar.js
@@ -3,6 +3,7 @@
   let birdDataGlobal = null;
   let birdDataGlobalParsed = null;
   let targetDate = [];
+  let targetDateNoTraining = [];
 
 
 //Get the data using the json_script -  selected_bird_json|json_script:"selected_bird_data" 
@@ -24,10 +25,15 @@ document.addEventListener('DOMContentLoaded', () => {
 document.addEventListener('DOMContentLoaded', () => {
   if(birdDataGlobal){
   birdDataGlobalParsed = JSON.parse(birdDataGlobal);
+  console.log(birdDataGlobalParsed);
   birdDataGlobalParsed.forEach((trainingData) => {
   if(trainingData.training) {
     targetDate.push(trainingData.date.slice(0, 10));
-  }
+  } else if 
+    (trainingData.food_type && !trainingData.training) {
+      targetDateNoTraining.push(trainingData.date.slice(0, 10));
+    }
+  
 
 })
   }
@@ -46,15 +52,24 @@ document.addEventListener('DOMContentLoaded', () => {
           // Get the full date of the current button
           const fullDate = dateEl.getAttribute('data-vc-date');
 
-          // Apply styles to specific dates
+          // Apply styles to specific dates with training
           targetDate.forEach((targetDate) => {
               if (fullDate === targetDate) {
-                  btnEl.style.backgroundColor = 'green'; // Set background color
-                  btnEl.style.color = '#FFFFFF'; // Set text color
-                  btnEl.style.borderRadius = '10%'; // Make it circular
-                  btnEl.style.fontWeight = 'bold'; // Make the text bold
-              }
+                  btnEl.style.backgroundColor = 'green'; 
+                  btnEl.style.color = '#FFFFFF'; 
+                  btnEl.style.borderRadius = '10%'; 
+                  btnEl.style.fontWeight = 'bold'; 
+              } 
           });
+          // Apply styles to specific dates with no training 
+          targetDateNoTraining.forEach((targetDate) => {
+            if (fullDate === targetDate) {
+                btnEl.style.backgroundColor = 'orange'; 
+                btnEl.style.color = '#FFFFFF'; 
+                btnEl.style.borderRadius = '10%'; 
+                btnEl.style.fontWeight = 'bold'; 
+            } 
+        });
       },
   };
 
