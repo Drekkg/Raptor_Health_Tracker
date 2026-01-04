10f7c1cab5160352e22ef00e408b8fc8ccd2a909
drekkg <rizzagum@gmail.com>
2026-01-04 09:59:28 +0100

    CHANGE update imports in forms and views, and clean up settings file

--- raptor_health/settings.py
+++ raptor_health/settings.py
@@ -25,6 +25,8 @@ import cloudinary.uploader
 import cloudinary.api
 
 
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
@@ -161,6 +163,7 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
+
 
 
 
